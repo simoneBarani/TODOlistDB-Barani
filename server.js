c33aac38978bb523824781545289c15d069e4556
Simone Barani <baranisimone@itis-molinari.eu>
2025-03-01 11:45:10 +0100

    inizio server

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const http = require("http");
+const fs = require("fs");
+const mysql = require("mysql");
